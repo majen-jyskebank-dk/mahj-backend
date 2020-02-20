eb592bd3cd4c8913ad2cf961d7b2b14cce98c15d
Mads A. H. Jensen <Mads-AH-Jensen@live.dk>
2020-02-20 16:35:35 +0100

    Minor refactoring to logging pattern

--- src/utils/logger.util.js
+++ src/utils/logger.util.js
@@ -9,23 +9,23 @@ exports.addCorrelation = (req, res, next) => {
 };
 
 exports.server = (message) => {
-    if (config.enableServer) log('server', `[SERVER] ${ getTime() }: ${ message }`);
+    if (config.enableServer) log('server', `${ getTime() } [SERVER]: ${ message }`);
 };
 
 exports.serverError = (message, error) => {
-    if (config.enableServer) log('server', `[SERVER] ${ getTime() }: ${ message } | ${ error }`);
+    if (config.enableServer) log('server', `${ getTime() } [SERVER]: ${ message } | ${ error }`);
 };
 
 exports.audit = (req, res) => {
-    if (config.enableAudit) log('audit', `[AUDIT] ${ getTime() }: [${ req.method }] ${ req.url } | ${ res.statusCode } | ${ req.correlationId }`);
+    if (config.enableAudit) log('audit', `${ getTime() } [AUDIT]: [${ req.method }] ${ req.url } | ${ res.statusCode } | ${ req.correlationId }`);
 };
 
 exports.info = (req, res, message) => {
-    if (config.enableInfo) log('server', `[INFO] ${ getTime() }: ${ message } | ${ req.correlationId }`);
+    if (config.enableInfo) log('server', `${ getTime() } [INFO]: ${ message } | ${ req.correlationId }`);
 };
 
 exports.error = (req, res, message, error) => {
-    if (config.enableError) log('server', `[ERROR] ${ getTime() }: ${ message } | ${ error } | ${ req.correlationId }`);
+    if (config.enableError) log('server', `${ getTime() } [ERROR]: ${ message } | ${ error } | ${ req.correlationId }`);
 };
 
 const log = (type, message) => {
@@ -43,5 +43,5 @@ const log = (type, message) => {
 
 const getTime = () => {
     let now = new Date();
-    return `${ now.getFullYear() }-${ now.getMonth() + 1 }-${ now.getDate() } ${ now.getHours() }:${ now.getMinutes() }:${ now.getSeconds() },${ now.getMilliseconds() }`
+    return `${ now.getHours() }:${ now.getMinutes() }:${ now.getSeconds() },${ now.getMilliseconds() } ${ now.getDate() }-${ now.getMonth() + 1 }-${ now.getFullYear() }`
 };
