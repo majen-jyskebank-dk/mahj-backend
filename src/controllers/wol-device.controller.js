e156cd418b75d4f35ec972cc66b8b24c5b5ec7d4
Mads A. H. Jensen <Mads-AH-Jensen@live.dk>
2020-03-10 07:54:16 +0100

    Implemented create, update and delete functionality for WOL sockets

--- src/controllers/wol-device.controller.js
+++ src/controllers/wol-device.controller.js
@@ -4,40 +4,52 @@ const WolDevice = require('../models/wol-device.model');
 const ping = require('../utils/ping.util');
 const wol = require('../utils/wol.util');
 
-exports.get = async (_id) => {
+exports.getLean = async (_id) => {
     return await WolDevice.findOne({ _id }).lean().exec();
 };
+
+exports.get = async (_id) => {
+    return await WolDevice.findById(_id).lean().exec();
+}
 
 exports.list = async () => {
     return await WolDevice.find().lean().exec();
 };
 
-exports.post = async (wolDevice) => {
-    return await new WolDevice({ 
+exports.create = async (wolDevice) => {
+    return (await new WolDevice({ 
         name: wolDevice.name,
         macAddress: wolDevice.macAddress,
         localIpAddress: wolDevice.localIpAddress,
         sshEnabled: wolDevice.sshEnabled,
         icon: wolDevice.icon
-     }).save();
+     }).save()).toObject();
+};
+
+exports.update = async (data) => {
+    return await WolDevice.findByIdAndUpdate(data._id,
+        {
+            name: data.name,
+            macAddress: data.macAddress,
+            localIpAddress: data.localIpAddress,
+            sshEnabled: data.sshEnabled,
+            icon: data.icon
+        }, { useFindAndModify: false }).lean().exec();
+};
+
+exports.delete = async (_id) => {
+    return await WolDevice.findByIdAndDelete(_id, { useFindAndModify: false }).lean().exec();
 };
 
 exports.wake = async (_id) => {
-    let wolDevice = await this.get(_id);
+    let wolDevice = await this.getLean(_id);
     return await wol.wake(wolDevice.macAddress);
 };
 
 exports.status = async(_id) => {
-    let wolDevice = await this.get(_id);
+    let wolDevice = await this.getLean(_id);
+
+    if (!wolDevice) throw new Error(`No WOL device found with ID: ${ _id }`);
+
     return await ping.isAlive(wolDevice.localIpAddress);
-}
-
-exports.pollStatus = async (_id) => {
-    for (let i = 0; i < config.pollTries; i++) {
-        if (await this.status(_id)) {
-            return true;
-        }
-    }
-
-    return false;
-}
+};
--- src/sockets/wol-device.socket.js
+++ src/sockets/wol-device.socket.js
@@ -4,37 +4,56 @@ const authentication = require('../authentication');
 const wolDevices = require('../controllers/wol-device.controller');
 
 module.exports = async (io) => {
-    const wolDevicesList = await wolDevices.list();
-
     io.on('connection', (socket) => {
         logger.audit({ socket, action: 'ON:connection', message: 'Established socket connection' });
         
-        socket.on('authenticate', (data) => {
-            socket.shouldPoll = true;
+        socket.on('disconnect', () => {
+            socket.shouldPoll = false;
+            logger.audit({ socket, action: 'ON:disconnect', message: 'Socket disconnected' });
+        });
+        
+        socket.on('authenticate', async (data) => {
+            logger.audit({ socket, action: 'ON:authenticate', message: 'Attempting to authenticate socket' });
 
             try {
-                if (authentication.verify(data.token)) {
-                    socket.emit('authenticated', logger.audit({ socket, action: 'EMIT:authenticated', message: 'Authenticated socket connection' }));
-    
-                    wolDevicesList.forEach(wolDevice => {
-                        pollStatus(socket, wolDevice._id);
-                    });
-    
-                    socket.on('wakeWolDevice', (data) => {
-                        logger.audit({ socket, action: 'ON:wakeWolDevice', message: `Wake ${ data._id }` });
-                        wolDevices.wake(data._id);
-                    });
+                if (!authentication.verify(data.token)) {
+                    throw new Error('Invalid token');
                 }
             } catch (err) {
-                socket.disconnect();
                 logger.info({ socket, message: `Couldn't authenticate socket` });
-                socket.shouldPoll = false;
+                socket.disconnect();
+                return;
             }
-            
 
-            socket.on('disconnect', () => {
-                socket.shouldPoll = false;
-                logger.audit({ socket, action: 'ON:disconnect', message: 'Socket disconnected' });
+            socket.emit('authenticated', logger.audit({ socket, action: 'EMIT:authenticated', message: 'Authenticated socket connection' }));
+            socket.shouldPoll = true;
+            (await wolDevices.list()).forEach(wolDevice => {
+                pollStatus(socket, wolDevice._id);
+            });
+
+            socket.on('createWolDevice', async (data) => {
+                logger.audit({ socket, action: 'ON:createWolDevice', message: 'Create WOL device' });
+                let wolDevice = await wolDevices.create(data);
+                socket.emit('createdWolDevice', { _id: wolDevice._id });
+                logger.audit({ socket, action: 'EMIT:createdWolDeice', message: `Created WOL device ${ wolDevice._id }` });
+                pollStatus(socket, wolDevice._id);
+            });
+
+            socket.on('updateWolDevice', async (data) => {
+                logger.audit({ socket, action: 'ON:updateWolDevice', message: `Update WOL device ${ data._id }`});
+                socket.emit('updatedWolDevice', { wolDevice: await wolDevices.update(data) });
+            });
+
+            socket.on('deleteWolDevice', async (data) => {
+                logger.audit({ socket, action: 'ON:deleteWolDevice', message: `Delete WOL device ${ data._id }` });
+                await wolDevices.delete(data._id);
+                socket.emit('deletedWolDevice', { _id: data._id });
+                logger.audit({socket, action: 'EMIT:deletedWolDevice', message: `Deleted WOL device ${ data._id }`});
+            });
+
+            socket.on('wakeWolDevice', (data) => {
+                logger.audit({ socket, action: 'ON:wakeWolDevice', message: `Wake ${ data._id }` });
+                wolDevices.wake(data._id);
             });
         });
     });
@@ -45,13 +64,19 @@ const pollStatus = async (socket, _id) => {
     let currentState, previousState;
 
     while (socket.shouldPoll) {
-        currentState = await wolDevices.status(_id);
-        if (currentState != previousState) {       
-            previousState = currentState;
-            socket.emit('statusUpdate', { _id, isAwake: currentState });
-            logger.audit({ socket, action: 'EMIT:statusUpdate', message: `Status update to ${ currentState } for ${ _id }` });
+        try {
+            currentState = await wolDevices.status(_id);
+
+            if (currentState != previousState) {       
+                previousState = currentState;
+                socket.emit('statusUpdate', { _id, isAwake: currentState });
+                logger.audit({ socket, action: 'EMIT:statusUpdate', message: `Status update to ${ currentState } for ${ _id }` });
+            }
+            await sleep(1000);
+        } catch (err) {
+            logger.serverError(`Caught error while polling status for ${ _id }`, err);
+            return;
         }
-        await sleep(1000);
     }
 }
 
